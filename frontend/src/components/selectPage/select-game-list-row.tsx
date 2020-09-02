188e84a2cbbc9a8bd09d6fe26d73635cc19216c5
GatiAher <2gatiaher@gmail.com>
2020-09-02 16:20:37 -0400

    Format date

--- frontend/src/components/selectPage/select-game-list-row.tsx
+++ frontend/src/components/selectPage/select-game-list-row.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { GameType } from "../../types/game-type";
+import { DAYS } from "../../maps/date-format";
 import "./select-game-list.css";
 
 interface TeamButtonProps {
@@ -31,22 +32,31 @@ interface SelectGameListRowProps {
   isTeamSelected: (team: string) => boolean;
 }
 
-export const SelectGameListRow = (props: SelectGameListRowProps) => (
-  <tr className="table-row">
-    <td className="table-item">{props.game.startTime}</td>
-    <td className="table-item">
-      <TeamButton
-        team={props.game.visTeam}
-        handleTeamSelect={props.handleTeamSelect}
-        isTeamSelected={props.isTeamSelected}
-      />
-    </td>
-    <td className="table-item">
-      <TeamButton
-        team={props.game.homeTeam}
-        handleTeamSelect={props.handleTeamSelect}
-        isTeamSelected={props.isTeamSelected}
-      />
-    </td>
-  </tr>
-);
+export const SelectGameListRow = (props: SelectGameListRowProps) => {
+  const dateObj = new Date(props.game.startTime);
+  const day = DAYS.get(dateObj.getDay());
+  const yearRegex = /(\/[^\/]+$)/;
+  const date = dateObj.toLocaleDateString().replace(yearRegex, "");
+  const secondsRegex = /(:[\d]+ )/;
+  const time = dateObj.toLocaleTimeString().replace(secondsRegex, " ");
+
+  return (
+    <tr className="table-row">
+      <td className="table-item">{`${day} ${date} ${time}`}</td>
+      <td className="table-item">
+        <TeamButton
+          team={props.game.visTeam}
+          handleTeamSelect={props.handleTeamSelect}
+          isTeamSelected={props.isTeamSelected}
+        />
+      </td>
+      <td className="table-item">
+        <TeamButton
+          team={props.game.homeTeam}
+          handleTeamSelect={props.handleTeamSelect}
+          isTeamSelected={props.isTeamSelected}
+        />
+      </td>
+    </tr>
+  );
+};
