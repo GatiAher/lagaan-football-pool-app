5c95dd148ac41b4c772a3d3133704ed88adf4e26
GatiAher <2gatiaher@gmail.com>
2020-09-03 12:57:20 -0400

    Add home and admin routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, NavLink } from "react-router-dom";
 import "./styles/App.css";
+import Home from "./routes/home";
+import Admin from "./routes/admin";
 import Game from "./routes/game";
 import Select from "./routes/select";
 import Leaderboard from "./routes/leaderboard";
@@ -8,13 +10,21 @@ import Leaderboard from "./routes/leaderboard";
 const App = () => (
   <BrowserRouter>
     <div>
-      <h1>Simple SPA</h1>
+      <h1>Lagaan Football Pool</h1>
       <nav>
         <ul className="header">
           <li>
             <NavLink exact to="/">
-              Leaderboard
+              Home
             </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/admin">
+              Admin
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/leaderboard">Leaderboard</NavLink>
           </li>
           <li>
             <NavLink to="/select">Select</NavLink>
@@ -26,14 +36,20 @@ const App = () => (
       </nav>
       <div className="content">
         <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/admin">
+            <Admin />
+          </Route>
+          <Route path="/leaderboard">
+            <Leaderboard />
+          </Route>
           <Route path="/select">
             <Select />
           </Route>
           <Route path="/game">
             <Game />
-          </Route>
-          <Route path="/">
-            <Leaderboard />
           </Route>
         </Switch>
       </div>
