652cd1ab0957532a425faca404934b0ca6faa267
GatiAher <2gatiaher@gmail.com>
2020-09-08 14:27:01 -0400

    Implement ranked leaderboard that is aware of current user

---
+++ frontend/src/components/Leaderboard/Leaderboard.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState, useCallback } from "react";
+import axios from "axios";
+import { LeaderboardList } from "./LeaderboardList";
+import "./Leaderboard.css";
+
+const fetchUsers = async (
+  setUsers: (arg0: any) => void,
+  setLoading: (arg0: boolean) => void
+) => {
+  axios
+    .get(`/user/ranked`)
+    .then((response) => {
+      setUsers(response.data);
+      setLoading(false);
+    })
+    .catch((error) =>
+      console.error(`There was an error retrieving the game list: ${error}`)
+    );
+};
+
+export const Leaderboard = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchUsersCallback = useCallback(() => {
+    fetchUsers(setUsers, setLoading);
+  }, []);
+
+  // Fetch all books on initial render
+  useEffect(() => {
+    fetchUsersCallback();
+  }, [fetchUsersCallback]);
+
+  return (
+    <div className="game-list-wrapper">
+      {/* Form for creating new book */}
+      <LeaderboardList users={users} loading={loading} />
+    </div>
+  );
+};
--- frontend/src/routes/leaderboard.tsx
+++ frontend/src/routes/leaderboard.tsx
@@ -1,18 +1,11 @@
 import React from "react";
+import { Leaderboard } from "../components/Leaderboard/Leaderboard";
 
-const Leaderboard = () => (
+const LeaderboardPage = () => (
   <div>
     <h2>Leaderboard</h2>
-    <p>
-      Cras facilisis urna ornare ex volutpat, et convallis erat elementum. Ut
-      aliquam, ipsum vitae gravida suscipit, metus dui bibendum est, eget
-      rhoncus nibh metus nec massa. Maecenas hendrerit laoreet augue nec
-      molestie. Cum sociis natoque penatibus et magnis dis parturient montes,
-      nascetur ridiculus mus.
-    </p>
-
-    <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+    <Leaderboard />
   </div>
 );
 
-export default Leaderboard;
+export default LeaderboardPage;
