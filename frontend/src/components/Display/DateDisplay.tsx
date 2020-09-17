1a80d40538242be7544cdf673a4771610e466b56
GatiAher <2gatiaher@gmail.com>
2020-09-16 23:48:14 -0400

    Fix color styling

--- frontend/src/components/Display/DateDisplay.tsx
+++ frontend/src/components/Display/DateDisplay.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { Box } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 import { dateParser } from "../../utils/date-parser";
 
 const DateDisplay = (props: { miliseconds: number }) => {
   const dateObj = dateParser(props.miliseconds);
+  const isOver = Date.now() > props.miliseconds;
+  const textColor = isOver ? "textSecondary" : "textPrimary";
   return (
     <Box display="flex" flexDirection="column">
-      <Box>{`${dateObj.day} ${dateObj.date}`}</Box>
-      <Box>{dateObj.time}</Box>
+      <Typography
+        color={textColor}
+        variant="body2"
+      >{`${dateObj.day} ${dateObj.date}`}</Typography>
+      <Typography color={textColor} variant="body2">
+        {isOver ? "DONE" : dateObj.time}
+      </Typography>
     </Box>
   );
 };
--- frontend/src/components/SeasonGame/SeasonGameListRow.tsx
+++ frontend/src/components/SeasonGame/SeasonGameListRow.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import { useTheme } from "@material-ui/core/styles";
 import {
   Box,
   List,
@@ -14,35 +15,30 @@ import DateDisplay from "../Display/DateDisplay";
 import TeamLogo from "../Display/TeamLogo";
 import Typography from "@material-ui/core/Typography";
 
-const useStyles = makeStyles({
-  neutral: {},
-  lose: {
-    color: "#a3a5a8",
-  },
-});
-
-const getTableItemClassName = (classes: any, status: number): string => {
-  if (status === 2 || status === -1) {
-    return classes.neutral;
-  } else {
-    return classes.lose;
-  }
+const TeamListStatusIndicatorIcon = (props: { status: number }) => {
+  const theme = useTheme();
+  const colorArrow =
+    props.status !== 2
+      ? theme.palette.background.default
+      : theme.palette.common.black;
+  return <ArrowLeftIcon style={{ color: colorArrow }} />;
 };
 
 const TeamListItem = (props: {
   team: string;
   points: number;
   status: number;
-  startTime: number;
-  classes: any;
 }) => {
-  const classStatusName = getTableItemClassName(props.classes, props.status);
+  const colorText =
+    props.status === 2 || props.status === -1 ? "textPrimary" : "textSecondary";
   return (
-    <ListItem className={classStatusName} button={false} style={{ padding: 0 }}>
+    <ListItem color={colorText} button={false} style={{ padding: 0 }}>
       <ListItemAvatar>
         <TeamLogo team={props.team} />
       </ListItemAvatar>
-      <ListItemText primary={props.team} />
+      <ListItemText>
+        <Typography color={colorText}>{props.team}</Typography>
+      </ListItemText>
       {props.status !== -1 && (
         <ListItemSecondaryAction
           style={{
@@ -53,8 +49,8 @@ const TeamListItem = (props: {
             padding: 0,
           }}
         >
-          <Typography className={classStatusName}>{props.points}</Typography>
-          <ArrowLeftIcon className={classStatusName} />
+          <Typography color={colorText}>{props.points}</Typography>
+          <TeamListStatusIndicatorIcon status={props.status} />
         </ListItemSecondaryAction>
       )}
     </ListItem>
@@ -62,31 +58,27 @@ const TeamListItem = (props: {
 };
 
 export const SeasonGameListRow = (props: { game: GameType }) => {
-  const classes = useStyles();
+  const theme = useTheme();
   return (
     <Box
       display="flex"
       flexDirection="row"
       justifyContent="space-between"
-      bgcolor="white"
+      bgcolor={theme.palette.background.default}
       py={4}
       pl={3}
     >
-      <Box borderRight={1} width="60%" p={0}>
+      <Box borderRight={1} color={theme.palette.grey.A200} width="60%" p={0}>
         <List dense={true} style={{ padding: 0, margin: 0 }}>
           <TeamListItem
             team={props.game.visTeam}
             points={props.game.visPts}
             status={props.game.visStatus}
-            startTime={props.game.startTime}
-            classes={classes}
           />
           <TeamListItem
             team={props.game.homeTeam}
             points={props.game.homePts}
             status={props.game.homeStatus}
-            startTime={props.game.startTime}
-            classes={classes}
           />
         </List>
       </Box>
