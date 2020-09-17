3326a592c9edfa3a7c1b1933ef59b53fb21beec4
GatiAher <2gatiaher@gmail.com>
2020-09-16 21:53:11 -0400

    Remove unused styling

--- frontend/src/components/SeasonGame/SeasonGameList.tsx
+++ frontend/src/components/SeasonGame/SeasonGameList.tsx
@@ -1,21 +1,10 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import ListSubheader from "@material-ui/core/ListSubheader";
 import withWidth from "@material-ui/core/withWidth";
-import Box from "@material-ui/core/Box";
 import { SeasonGameListRow } from "./SeasonGameListRow";
 import { GameType } from "../../utils/types/game-type";
-
-const useStyles = makeStyles({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    overflow: "hidden",
-  },
-});
 
 interface SeasonGameListProps {
   games: GameType[];
@@ -24,26 +13,23 @@ interface SeasonGameListProps {
 }
 
 function SeasonGameList(props: SeasonGameListProps): JSX.Element {
-  const classes = useStyles();
   let gridListCols = props.width === "xs" ? 1 : 2;
   if (props.loading) return <p>Game table is loading...</p>;
   return (
-    <Box className={classes.root}>
-      <GridList
-        cellHeight="auto"
+    <GridList cellHeight="auto" cols={gridListCols}>
+      <GridListTile
+        key="Subheader"
         cols={gridListCols}
-        style={{ overflow: "hidden" }}
+        style={{ height: "auto" }}
       >
-        <GridListTile key="Subheader" cols={2} style={{ height: "auto" }}>
-          <ListSubheader component="div">WEEK</ListSubheader>
+        <ListSubheader component="div">WEEK</ListSubheader>
+      </GridListTile>
+      {props.games.map((game: GameType) => (
+        <GridListTile key={game.game_id}>
+          <SeasonGameListRow game={game} />
         </GridListTile>
-        {props.games.map((game: GameType) => (
-          <GridListTile key={game.game_id}>
-            <SeasonGameListRow game={game} />
-          </GridListTile>
-        ))}
-      </GridList>
-    </Box>
+      ))}
+    </GridList>
   );
 }
 
