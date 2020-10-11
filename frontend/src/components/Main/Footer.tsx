955a7d090ff62a3d77d061b2f347f2c21b37df30
GatiAher <2gatiaher@gmail.com>
2020-10-11 19:28:27 -0400

    Add sticky footer

--- frontend/src/components/Main/Footer.tsx
+++ frontend/src/components/Main/Footer.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 
 import { AppBar, Typography, Box } from "@material-ui/core";
+import { BorderTop } from "@material-ui/icons";
 
 export default () => {
   return (
-    <Box flexGrow={1} pt={5}>
-      <AppBar position="static">
+    <Box
+      flexGrow={1}
+      style={{
+        marginTop: "calc(5% + 60px)",
+        bottom: 0,
+      }}
+    >
+      <AppBar position="sticky">
         <Box p={2} m="auto">
           <Typography variant="subtitle1">© 2020 Gati Aher</Typography>
         </Box>
