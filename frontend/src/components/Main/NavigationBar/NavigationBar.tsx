60c0af470d4a08c6a8d31fbcaa629074fd23d2fc
GatiAher <2gatiaher@gmail.com>
2020-10-13 02:04:37 -0400

    Change header name to Lagaan Football

--- frontend/src/components/Main/NavigationBar/NavigationBar.tsx
+++ frontend/src/components/Main/NavigationBar/NavigationBar.tsx
@@ -79,7 +79,7 @@ const NavigationBar: React.FC<RouteComponentProps> = ({
               <MenuIcon />
             </IconButton>
             <Box flexGrow={1}>
-              <Typography variant="h6">Football Almanac</Typography>
+              <Typography variant="h6">Lagaan Football</Typography>
             </Box>
             <AuthNav />
           </Toolbar>
