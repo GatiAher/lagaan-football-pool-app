e9b2052b565a81ac75f296ab47fb3509c38454d2
GatiAher <2gatiaher@gmail.com>
2020-11-11 21:26:19 -0500

    Add GitHub Icon to footer

--- frontend/src/layout/Footer.tsx
+++ frontend/src/layout/Footer.tsx
@@ -1,23 +1,42 @@
 import React from "react";
-import { useTheme } from "@material-ui/core";
+import { Button, IconButton, useTheme } from "@material-ui/core";
 
 import Box from "@material-ui/core/Box";
+
+import GitHubIcon from "@material-ui/icons/GitHub";
 
 export default () => {
   const theme = useTheme();
   return (
     <Box
-      flexGrow={1}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
       height="80px"
       style={{
         position: "absolute",
         bottom: 0,
-        width: "100%"
+        width: "100%",
       }}
       bgcolor={theme.palette.primary.light}
     >
-      <Box p={2} m="auto" textAlign="center" color={theme.palette.grey[100]}>
-        © 2020 Gati Aher
+      <Box>
+        <Button
+          variant="text"
+          style={{ color: theme.palette.grey[100] }}
+          startIcon={
+            <GitHubIcon
+              fontSize="small"
+              style={{ color: theme.palette.grey[100] }}
+            />
+          }
+          onClick={() => {
+            window.location.href =
+              "https://github.com/GatiAher/lagaan-football-pool-app";
+          }}
+        >
+          <Box pt="3px">2020 Gati Aher</Box>
+        </Button>
       </Box>
     </Box>
   );
