3eb64b331c80f871c2b2df75b3a2d9972189e925
Gati Aher <2gatiaher@gmail.com>
2021-11-28 15:41:52 -0800

    Revert currentWeek logic

--- admin/src/contexts/calculateCurrentWeek.ts
+++ admin/src/contexts/calculateCurrentWeek.ts
@@ -1,12 +1,15 @@
 const calculateCurrentWeek = (kickoffMs: number): number => {
-  // week changes on Thursday, but currentWeek changes on midnight Monday
+  // week changes on Thursday, but currentWeek changes on Sun  @ 1pm
   const currentDateObj = new Date();
   const diff = (currentDateObj.getTime() - kickoffMs) / 1000;
   const weeksDiff = diff / (60 * 60 * 24 * 7);
   let weeks = Math.ceil(weeksDiff);
-  // show next week if Tue or Wed
+  // show next week if Sun after 1pm, Mon, Tue, or Wed
   if (
-    currentDateObj.getDay() === 2 || currentDateObj.getDay() === 3
+    (currentDateObj.getDay() === 0 && currentDateObj.getHours() >= 13) ||
+    currentDateObj.getDay() === 1 ||
+    currentDateObj.getDay() === 2 ||
+    currentDateObj.getDay() === 3
   )
     weeks = weeks + 1;
   if (weeks > 0 && weeks <= 21) return weeks;
